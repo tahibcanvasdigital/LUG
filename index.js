928da8e56f8def36a59c46b3220a0277a49d05fd
tahibcanvasdigital <muhammad.tahib@canvasdigital.net>
2024-01-04 13:28:09 +0500

    inital

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { UserModel } from "./models/user.js";
+
+
+const app = express();
+
+dotenv.config();
+app.use(cors());
+
+app.get("/api/login", (req, res) => {
+  const data = [
+    {
+    name: "ali",
+    },
+  ];
+  res.send(data);
+});
+app.use(express.json());
+app.post("/api/contactus", async (req, res) => {
+  try {
+    const {username, email, message } = req.body;
+    const newUser = new UserModel({username, email, message });
+    const savedUser = await newUser.save();
+    res.json(savedUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+mongoose.connect("mongodb+srv://muhammadtahib:Sybrid098@canvasdigital.rvnqiwu.mongodb.net/");
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Port Running ${PORT}`);
+});
---
+++ models/user.js
@@ -0,0 +1,18 @@
+import  { mongoose } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  }
+});
+
+export const UserModel = mongoose.models.usermodel || mongoose.model('usermodel', userSchema);
